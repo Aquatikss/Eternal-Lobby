25883ea4bda5e526799a82945d0b69f6d753a1f0
Aquatikss <tiaanbotha080818@gmail.com>
2025-05-22 12:32:04 +0100

    Add Minestom integration and configure server initialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.gradleup.shadow") version "8.3.0"
 }
 
 group = "com.aquamobs"
@@ -10,11 +11,34 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("net.minestom:minestom-snapshots:f62abc722f")
+    implementation("net.minestom:minestom-snapshots:ebaa2bbf64")
+    implementation("com.googlecode.json-simple:json-simple:1.1.1")
+    implementation("dev.hollowcube:schem:1.3.1")
+    implementation("org.slf4j:slf4j-nop:2.0.17")
+    implementation("org.fusesource.jansi:jansi:2.4.1")
+    implementation("net.kyori:adventure-text-minimessage:4.14.0")
 }
 
-tasks.test {
-    useJUnitPlatform()
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes["Main-Class"] = "com.aquamobs.Main"
+        }
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+    shadowJar {
+        mergeServiceFiles()
+        archiveClassifier.set("")
+        destinationDirectory.set(file("R:/"))
+        archiveFileName.set("server.jar")
+    }
 }
